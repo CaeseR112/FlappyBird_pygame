c90fd8435f8439070e58eba1a335a32f9ceb3f5f
kusal jayashanka <104189503+CaeseR112@users.noreply.github.com>
2024-02-18 12:49:47 +0530

    Background of the game
    Floor and the sky images implemented

--- FlappyBird.py
+++ FlappyBird.py
@@ -3,24 +3,41 @@ from sys import exit
 
 #pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('No Internet') 
-clock = pygame.time.Clock()  #Setting the celing and the floor rates
+clock = pygame.time.Clock()  #Setting the celing and the floor frame rates
 
-#Setting the window icon
-img = pygame.image.load('icon.png')
-pygame.display.set_icon(img)
+################### Font declaration ##############################
 
-test_surface = pygame.Surface((1250,700))  #Creation of a surface - wont display yet
+test_font = pygame.font.Font( None, 50)
+
+
+
+test_surface = pygame.Surface((100,200))
 test_surface.fill('Red')
+
+####################### Image Declaration ##########################
+
+Sky_Image = pygame.image.load('Graphics/Backgrounds/Sky.png')
+Floor_Image = pygame.image.load('Graphics/Backgrounds/ground.png')
+
+###################### Text surfaces ################################
+
+text_surface = test_font.render('Lolla', True ,'White')
+
+    
 
 #Quitting pygame window
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            pygame.quit() 
             exit()
-    screen.blit(test_surface,(100,100))
+
+    screen.blit(Sky_Image,(0,0))
+    screen.blit(Floor_Image,(0,300))
+    screen.blit(test_surface,(300,50))
+
     pygame.display.update()
     clock.tick(60)
 
